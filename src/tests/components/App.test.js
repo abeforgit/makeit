00c121b7c1b882f75edcbc3aef7cff9b147b0243
abeforgit <arnebertrand@gmail.com>
2018-02-17 23:34:51 +0100

    Added a full app render test

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -3,10 +3,16 @@ import App from '../../components/App';
 import routes from '../../routes/index';
 import rootReducer from '../../store/reducers/index';
 import { createStore } from 'redux';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 const store = createStore(rootReducer);
 
-it('renders without crashing', () => {
-  shallow(<App store={store} routes={routes(store)} />);
+describe('<App />', () => {
+  test('shallow renders without crashing', () => {
+    shallow(<App store={store} routes={routes(store)} />);
+  });
+
+  test('full renders without crashing', () => {
+    mount(<App store={store} routes={routes(store)} />);
+  });
 });
