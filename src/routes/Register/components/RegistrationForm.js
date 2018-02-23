7faffffa30474ad01b04b9aa0250f72b964399ca
abeforgit <arnebertrand@gmail.com>
2018-02-23 16:10:56 +0100

    Setup flexbox for header and registration page, basic styling of header

--- src/routes/Register/components/RegistrationForm.js
+++ src/routes/Register/components/RegistrationForm.js
@@ -6,11 +6,16 @@ import TextBox from './TextBox';
 import PropTypes from 'prop-types';
 import EmailSubBox from './EmailSubBox';
 import TermsCondBox from './TermsCondBox';
+import styles from '../styles/RegistrationForm.style.css';
 
 const RegistrationForm = props => {
   const { handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit} method="POST">
+    <form
+      onSubmit={handleSubmit}
+      method="POST"
+      className={styles.registrationForm}
+    >
       <div>
         <Field
           name="email"
@@ -18,6 +23,7 @@ const RegistrationForm = props => {
           component={TextBox}
           type="text"
           validate={[required, validEmail]}
+          className={styles.inputGroup}
         />
       </div>
       <div>
@@ -27,6 +33,7 @@ const RegistrationForm = props => {
           component={TextBox}
           type="text"
           validate={required}
+          className={styles.inputGroup}
         />
       </div>
       <div>
@@ -36,6 +43,7 @@ const RegistrationForm = props => {
           component={TextBox}
           type="text"
           validate={required}
+          className={styles.inputGroup}
         />
       </div>
       <div>
@@ -45,6 +53,7 @@ const RegistrationForm = props => {
           component={TextBox}
           type="password"
           validate={required}
+          className={styles.inputGroup}
         />
       </div>
       <div>
@@ -54,6 +63,7 @@ const RegistrationForm = props => {
           component={TextBox}
           type="password"
           validate={[required, matchPW]}
+          className={styles.inputGroup}
         />
       </div>
       <div>
@@ -62,14 +72,24 @@ const RegistrationForm = props => {
           label="Country of residence"
           component={CountrySelection}
           validate={required}
+          className={styles.inputGroup}
         />
       </div>
       <div>
-        <Field name="emailSubscribe" component={EmailSubBox} />
+        <Field
+          name="emailSubscribe"
+          component={EmailSubBox}
+          className={styles.inputGroup}
+        />
       </div>
 
       <div>
-        <Field name="termsCond" component={TermsCondBox} validate={required} />
+        <Field
+          name="termsCond"
+          component={TermsCondBox}
+          validate={required}
+          className={styles.inputGroup}
+        />
       </div>
 
       <button type="submit">Submit</button>
